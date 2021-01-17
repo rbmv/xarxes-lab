53f8cdad6b21c2c62c064b9c45fd356fa8b6dc27
rbmv <rubenmartinez.vidal@gmail.com>
2021-01-17 13:48:46 +0100

    Do not provide quotes in basic run wo params

--- bash-env/waf-alias.sh
+++ bash-env/waf-alias.sh
@@ -4,7 +4,7 @@ NS3_EXTERNALS="/home/alumne/practiques/practica1/simulation-scripts"
 function ns3 {
 CWD="$PWD"
 cd $NS3CUR >/dev/null
-./waf --cwd="$CWD" "$*"
+./waf --cwd="$CWD" $*
 cd - >/dev/null
 }
 function ns3-run-wparams {
