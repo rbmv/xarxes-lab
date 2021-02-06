7df6a35dd0aa15c3fe1d2f1294258fe0867e38e8
rbmv <rubenmartinez.vidal@gmail.com>
2021-02-06 22:14:59 +0100

    Fixes some issues adds get-student info to PATH

--- bash-env/uab-env.sh
+++ bash-env/uab-env.sh
@@ -1,5 +1,5 @@
 . $HOME/.uab-env/waf-alias.sh
-export PATH="$HOME/.tools:$PATH"
+export PATH="$HOME/.tools:$HOME/.uab-env/env-tools/:$PATH"
 echo -e "Loading UAB Lab environment: \e[32m[SUCCESS]\e[39m"
-$HOME/.uab-env/get-student-info.sh
-[ -f "$HOME/.uab-env/alumne-env.sh"] && . $HOME/.uab-env/alumne-env.sh && echo -e "Loading Student info: \e[32m[SUCCESS]\e[39m"
+$HOME/.uab-env/env-tools/get-student-info.sh
+[ -f "$HOME/.uab-env/alumne-env.sh" ] && . $HOME/.uab-env/alumne-env.sh && echo -e "Loading Student info: \e[32m[SUCCESS]\e[39m"
