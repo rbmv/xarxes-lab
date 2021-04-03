854d2947c14af82d32758e6b9c1629ad5d4f4b37
rbmv <rubenmartinez.vidal@gmail.com>
2021-04-03 15:13:32 +0200

    Fixes issue in make-question echo was a literal hash was not changing according to group

--- tools/bin/make-question-sheet.sh
+++ tools/bin/make-question-sheet.sh
@@ -63,7 +63,7 @@ template=$HOME/.updates/templates/lab$numPrac/answerTemplate-Pr$numPrac.txt
 
 [ ! -f $template ] && echo "No template available for Practica $numPrac" && exit 1
 
-REF=`echo 'Pr${numPrac}${GRUP}${SUBGRUP}' | sha256sum | cut -d" " -f1`
+REF=`echo "Pr${numPrac}${GRUP}${SUBGRUP}" | sha256sum | cut -d" " -f1`
 
 echo "Grup: $GRUP$SUBGRUP" > ${fname}
 echo "NIU: $NIU1 - Alumne 1: $NOM1" >> ${fname}
